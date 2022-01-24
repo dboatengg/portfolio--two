2e987c40f48a04a01694d8300d108941d3828625
Boateng Dickson <89385798+dboatengg@users.noreply.github.com>
2022-01-24 04:54:28 -0800

    lock body when nav menu is active

--- main.js
+++ main.js
@@ -7,12 +7,17 @@ hamburger.addEventListener("click", mobileMenu);
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  document.body.classList.toggle('lock-scroll')
 }
 
 
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", removeOverflow));
 
 function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
+}
+function removeOverflow() {
+  document.body.classList.remove('lock-scroll')
 }
