e22b2c8791f4df11d99fd9459994c5c87af22dc0
Boateng Dickson <89385798+dboatengg@users.noreply.github.com>
2022-01-25 01:13:49 -0800

    General page tweak

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
+//Declare variables for hamburger and menu items
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav__menu");
 const navLink = document.querySelectorAll(".nav__link");
-
-hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
   document.body.classList.toggle('lock-scroll')
 }
+hamburger.addEventListener("click", mobileMenu);
 
-
+//Add a click event on to the close and each menu item to remove overflow effect
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
 navLink.forEach((n) => n.addEventListener("click", removeOverflow));
 
+
+//Add a function to remove close button
 function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+//add the overflow function
 function removeOverflow() {
   document.body.classList.remove('lock-scroll')
 }
