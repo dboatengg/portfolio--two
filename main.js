6c89f6ee71c6ed07ac704daa7cabfd0728945b89
Boateng Dickson <89385798+dboatengg@users.noreply.github.com>
2022-03-07 07:09:30 -0800

    Polist about section

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav__menu");
 const navLink = document.querySelectorAll(".nav__link");
+const darkBtn = document.querySelectorAll(".dark-btn");
+// const body = document.getElementsByTagName("body");
 
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-  document.body.classList.toggle('lock-scroll')
+  document.body.classList.toggle("lock-scroll");
 }
 hamburger.addEventListener("click", mobileMenu);
 
@@ -14,15 +16,18 @@ hamburger.addEventListener("click", mobileMenu);
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
 navLink.forEach((n) => n.addEventListener("click", removeOverflow));
 
-
 //Add a function to remove close button
 function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
 
-
 //add the overflow function
 function removeOverflow() {
-  document.body.classList.remove('lock-scroll')
+  document.body.classList.remove("lock-scroll");
 }
+
+// implement dark mode functionality
+darkBtn.addEventListener("click", () => {
+  document.body.classList.add("dark-mode");
+});
